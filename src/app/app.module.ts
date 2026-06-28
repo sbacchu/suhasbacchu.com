e361a3aaa5cf83c0353b4731e48412cbb762fd82
Suhas Bacchu <suhas.bacchu29@gmail.com>
2026-06-27 20:59:16 -0400

    refactor: sanitize email link and remove legacy testing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -13,7 +14,7 @@ import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent, ExperienceComponent, ContactComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
